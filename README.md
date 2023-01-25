6e1437b4b0e128ac88f88ef9483eeec791696d5f
shavy <shavygaming@gmail.com>
2023-01-25 15:29:09 +0530

    Fixes as per 2023

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # Clash-WhatsApp-Bot
 A Clash Of Clans Bot based on a WhatsApp client library for NodeJS that connects through the WhatsApp Web browser app.
+
+## Installation
+1. Install [NodeJS](https://nodejs.org/en/download/)
+2. Install [NPM](https://www.npmjs.com/get-npm)
+3. Install [Git](https://git-scm.com/downloads)
+4. Clone this repository using `git clone https://github.com/shavy4452/Clash-WhatsApp-Bot.git && cd Clash-WhatsApp-Bot`
+5. Install the dependencies using `npm install`
+6. Change the coc developer email and password in `config.json`
+7. Run the bot using `node client.js`
+8. Scan the QR code shown in the console using your WhatsApp
+9. Enjoy!
+
+## Usage
+1. Send `!clan` to get the clan information
+2. Send `!player` to get the player information
+
+Note: This is just an example, you can add more commands and features to the bot. You can also use this bot as a base for your own bot.
+
+## Credits
+- [Shavy](https://github.com/shavy4452/) - Developer
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
+
+
+## Contributing
+1. Fork the repository ([here is the guide](https://help.github.com/articles/fork-a-repo/))
+2. Create your feature branch (`git checkout -b feature/fooBar`)
+3. Commit your changes (`git commit -am 'Add some fooBar'`)
+4. Push to the branch (`git push origin feature/fooBar`)
+5. Create a new Pull Request
+6. Wait for your PR to be reviewed and merged
+7. Celebrate! :tada:
+
+## Contact
+- [Discord](https://discord.gg/BzbkwNdz)
+- [Email](mailto:shavygaming@gmail.com)
+
+## Donate
+- Send me a message on Discord or Email if you want to donate we accept donations through PayPal and Patreon and all other indian payment methods.
+
+## Disclaimer
+This project is not affiliated with WhatsApp or any of its partners in any way. WhatsApp is a registered trademark of WhatsApp Inc. This project is an independent and unofficial API. Use at your own risk.
+
+## Support
+If you need help with this project, you can contact me on Discord or Email.
--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const { Client } = require('whatsapp-web.js');
 const { Client: cocClient } = require('clashofclans.js');
-const { MongoClient } = require("mongodb");
-const axios = require('axios')
-const aysnc = require('async');
-const client = new Client({ puppeteer: { headless: false }});
 const coc = new cocClient();
+const config = require('./config.json');
+
+const client = new Client({
+    authStrategy: new LocalAuth(),
+});
 
 
 client.initialize();
@@ -46,7 +47,7 @@ async function clanInfo(tag){
 async function playerInfo(tag){
     try{
         let player = await coc.getPlayer(tag);
-        return player;
+        return player.name;
     }catch(err){
         console.log(err);
         if(err.status == 404){
@@ -60,186 +61,19 @@ async function playerInfo(tag){
 }
 
 client.on('message', async msg => {
-    if (msg.body === '!ping') {
-        msg.reply('pong');  // Reply to the message
-
-    } else if (msg.body.startsWith('!clan')){
-        // Clan Information
-        let chat = await msg.getChat();
-        var tag = msg.body.slice(6);
-        if(chat.isGroup){
-            if(tag.length ==0){
-                    return msg.reply('Please enter a valid tag!')
-                }
-            }
-            else{
-                clanInfo(tag).then(async (data) => {
-                    await msg.reply(data);
-                })
-            }
-        }else{
-            if(tag.length == 0){ 
-                    msg.reply('Please enter a valid tag!')
-                }   
-            else{
-                clanInfo(tag).then(async (data) => {
-                    await msg.reply(data);
-                })
-            }
+    var prefix = config.command_prefix;
+    var args = msg.body.slice(prefix.length).trim().split(/ +/g);
+    var command = args.shift().toLowerCase();
+    if (msg.body.startsWith(prefix)) {
+        if(command == 'clan'){
+            clanInfo(args[0]).then((res) => {
+                return msg.reply(res);
+            });
+        }else if(command == 'player'){
+            playerInfo(args[0]).then((res) => {
+                return msg.reply(res);
+            });
         }
-        
-    }else if (msg.body.startsWith('!subject')) {
-        // Change the group subject
-        let chat = await msg.getChat();
-        if (chat.isGroup) {
-            let newSubject = msg.body.slice(9);
-            chat.setSubject(newSubject);
-        } else {
-            msg.reply('This command can only be used in a group!');
-        }
-    } else if (msg.body.startsWith('!echo')) {
-        // Replies with the same message
-        msg.reply(msg.body.slice(6));
-    } else if (msg.body.startsWith('!desc')) {
-        // Change the group description
-        let chat = await msg.getChat();
-        if (chat.isGroup) {
-            let newDescription = msg.body.slice(6);
-            chat.setDescription(newDescription);
-        } else {
-            msg.reply('This command can only be used in a group!');
-        }
-    } else if (msg.body === '!leave') {
-        // Leave the group
-        let chat = await msg.getChat();
-        if (chat.isGroup) {
-            chat.leave();
-        } else {
-            msg.reply('This command can only be used in a group!');
-        }
-    } else if (msg.body.startsWith('!join')) {
-        const inviteCode = msg.body.split(' ')[1];
-        try {
-            await client.acceptInvite(inviteCode);
-            msg.reply('Joined the group!');
-        } catch (e) {
-            msg.reply('That invite code seems to be invalid.');
-        }
-    } else if (msg.body === '!groupinfo') {
-        let chat = await msg.getChat();
-        if (chat.isGroup) {
-            msg.reply(`
-                *Group Details*
-                Name: ${chat.name}
-                Description: ${chat.description}
-                Created At: ${chat.createdAt.toString()}
-                Created By: ${chat.owner.user}
-                Participant count: ${chat.participants.length}
-            `);
-        } else {
-            msg.reply('This command can only be used in a group!');
-        }
-    } else if (msg.body === '!chats') {
-        const chats = await client.getChats();
-        client.sendMessage(msg.from, `The bot has ${chats.length} chats open.`);
-    } else if (msg.body === '!info') {
-        let info = client.info;
-        client.sendMessage(msg.from, `
-            *Connection info*
-            User name: ${info.pushname}
-            My number: ${info.wid.user}
-            Platform: ${info.platform}
-        `);
-    } else if (msg.body === '!mediainfo' && msg.hasMedia) {
-        const attachmentData = await msg.downloadMedia();
-        msg.reply(`
-            *Media info*
-            MimeType: ${attachmentData.mimetype}
-            Filename: ${attachmentData.filename}
-            Data (length): ${attachmentData.data.length}
-        `);
-    } else if (msg.body === '!quoteinfo' && msg.hasQuotedMsg) {
-        const quotedMsg = await msg.getQuotedMessage();
-
-        quotedMsg.reply(`
-            ID: ${quotedMsg.id._serialized}
-            Type: ${quotedMsg.type}
-            Author: ${quotedMsg.author || quotedMsg.from}
-            Timestamp: ${quotedMsg.timestamp}
-            Has Media? ${quotedMsg.hasMedia}
-        `);
-    } else if (msg.body === '!resendmedia' && msg.hasQuotedMsg) {
-        const quotedMsg = await msg.getQuotedMessage();
-        if (quotedMsg.hasMedia) {
-            const attachmentData = await quotedMsg.downloadMedia();
-            client.sendMessage(msg.from, attachmentData, { caption: 'Here\'s your requested media.' });
-        }
-    } else if (msg.body === '!location') {
-        msg.reply(new Location(37.422, -122.084, 'Googleplex\nGoogle Headquarters'));
-    } else if (msg.location) {
-        msg.reply(msg.location);
-    } else if (msg.body.startsWith('!status ')) {
-        const newStatus = msg.body.split(' ')[1];
-        await client.setStatus(newStatus);
-        msg.reply(`Status was updated to *${newStatus}*`);
-    } else if (msg.body === '!mention') {
-        const contact = await msg.getContact();
-        const chat = await msg.getChat();
-        chat.sendMessage(`Hi @${contact.number}!`, {
-            mentions: [contact]
-        });
-    } else if (msg.body === '!delete') {
-        if (msg.hasQuotedMsg) {
-            const quotedMsg = await msg.getQuotedMessage();
-            if (quotedMsg.fromMe) {
-                quotedMsg.delete(true);
-            } else {
-                msg.reply('I can only delete my own messages');
-            }
-        }
-    } else if (msg.body === '!pin') {
-        const chat = await msg.getChat();
-        await chat.pin();
-    } else if (msg.body === '!archive') {
-        const chat = await msg.getChat();
-        await chat.archive();
-    } else if (msg.body === '!mute') {
-        const chat = await msg.getChat();
-        // mute the chat for 20 seconds
-        const unmuteDate = new Date();
-        unmuteDate.setSeconds(unmuteDate.getSeconds() + 20);
-        await chat.mute(unmuteDate);
-    } else if (msg.body === '!typing') {
-        const chat = await msg.getChat();
-        // simulates typing in the chat
-        chat.sendStateTyping();
-    } else if (msg.body === '!recording') {
-        const chat = await msg.getChat();
-        // simulates recording audio in the chat
-        chat.sendStateRecording();
-    } else if (msg.body === '!clearstate') {
-        const chat = await msg.getChat();
-        // stops typing or recording in the chat
-        chat.clearState();
-    } else if (msg.body === '!jumpto') {
-        if (msg.hasQuotedMsg) {
-            const quotedMsg = await msg.getQuotedMessage();
-            client.interface.openChatWindowAt(quotedMsg.id._serialized);
-        }
-    } else if (msg.body === '!buttons') {
-        let button = new Buttons('Button body',[{body:'bt1'},{body:'bt2'},{body:'bt3'}],'title','footer');
-        client.sendMessage(msg.from, button);
-    } else if (msg.body === '!list') {
-        let sections = [{title:'sectionTitle',rows:[{title:'ListItem1', description: 'desc'},{title:'ListItem2'}]}];
-        let list = new List('List body','btnText',sections,'Title','footer');
-        client.sendMessage(msg.from, list);
-    }
-});
-
-client.on('message_create', (msg) => {
-    // Fired on all message creations, including your own
-    if (msg.fromMe) {
-        // do stuff here
     }
 });
 
@@ -298,7 +132,6 @@ client.on('disconnected', (reason) => {
 });
 
 (async function () {
-    await coc.login({ email: 'devemail@gmail.com', password: 'xxxxx' });
-    const clan = await coc.getClan('#2PP');
-    console.log(`${clan.name} (${clan.tag})`);
+    await coc.login({ email: config.coc_dev_data.email, password: config.coc_dev_data.password });
+    console.log('Clash of Clans API Connected!');
 })();
